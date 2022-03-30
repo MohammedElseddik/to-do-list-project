939c0f3efc65dd8909f5455fcbea0b0dbd38451b
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-03-30 02:11:35 +0200

    Creat list class with list array and render method

--- src/index.js
+++ src/index.js
@@ -1,2 +1,59 @@
-import _ from 'lodash';
+import _, { add } from 'lodash';
 import './style.css';
+
+
+class ListProperties {
+
+    constructor(description, completed, index) {
+        this.description = description;
+        this.completed = completed;
+        this.index = index;
+    }
+}
+
+class List {
+
+    ListObjects = [
+        new ListProperties(
+            'complete the project',
+            true,
+            0
+        ),
+
+        new ListProperties(
+            'read the book',
+            true,
+            1
+        ),
+
+        new ListProperties(
+            'learn more about react',
+            false,
+            2
+        )
+    ];
+
+    render() {
+        const listBody = document.querySelector('.tasks-body');
+        for (const listItem of this.ListObjects) {
+            const addTaskBtn = document.createElement('button');
+            addTaskBtn.textContent = '+';
+            //addTaskBtn.addEventListener('click', addTask);
+            const listLi = document.createElement('li');
+            const text = document.createElement('p');
+            text.textContent = listItem.description;
+            text.className = 'list-description';
+            listLi.className = 'list-item';
+            const checkboxIcon = document.createElement('input');
+            checkboxIcon.type = 'checkbox';
+            listLi.appendChild(checkboxIcon);
+            listLi.appendChild(text);
+            listLi.appendChild(addTaskBtn);
+            listBody.appendChild(listLi);
+        }
+    }
+}
+
+const list1 = new List()
+list1.render();
+console.log(list1);
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
 
     plugins: [
@@ -16,7 +17,7 @@ module.exports = {
 
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist', 'scripts'),
     },
 
     module: {
