d30e1fa9927aad1d1ebfb30b646b1a2ef7621949
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-04-04 20:15:56 +0200

    fix linter issues

--- src/modules/List.js
+++ src/modules/List.js
@@ -2,43 +2,43 @@ import NewTask from './NewTask.js';
 import ListItem from './List item.js';
 
 export default class List {
-    constructor() {
-        this.ListObjects = localStorage.getItem('list') === null ? [] : JSON.parse(localStorage.getItem('list'));
-    }
+  constructor() {
+    this.ListObjects = localStorage.getItem('list') === null ? [] : JSON.parse(localStorage.getItem('list'));
+  }
 
-    addTask() {
-        const addTaskInput = document.querySelector('.add-task');
-        const listform = document.querySelector('.add__task');
-        if (addTaskInput.value.trim().length === 0) return;
-        this.ListObjects.push(new NewTask(addTaskInput.value, false));
-        listform.reset();
-        this.render();
-        localStorage.setItem('list', JSON.stringify(this.ListObjects));
-        this.completedStausCheck();
-    }
+  addTask() {
+    const addTaskInput = document.querySelector('.add-task');
+    const listform = document.querySelector('.add__task');
+    if (addTaskInput.value.trim().length === 0) return;
+    this.ListObjects.push(new NewTask(addTaskInput.value, false));
+    listform.reset();
+    this.render();
+    localStorage.setItem('list', JSON.stringify(this.ListObjects));
+    this.completedStausCheck();
+  }
 
-    selectTask(event, listLi, verticalDotsIcon, trashIcon) {
-        if (event.target.classList.contains('list-description')) {
-            listLi.classList.toggle('selected');
-            trashIcon.classList.toggle('hidden');
-            this.editTask(event.target);
-        } else if (event.target.classList.contains('trash-icon')) {
-            this.deleteTask(listLi, trashIcon);
-        }
+  selectTask(event, listLi, verticalDotsIcon, trashIcon) {
+    if (event.target.classList.contains('list-description')) {
+      listLi.classList.toggle('selected');
+      trashIcon.classList.toggle('hidden');
+      this.editTask(event.target);
+    } else if (event.target.classList.contains('trash-icon')) {
+      this.deleteTask(listLi, trashIcon);
     }
+  }
 
-    deleteTask(listLi, trashIcon) {
-        listLi.remove();
-        this.ListObjects.splice(trashIcon.id, 1);
-        this.render();
-        localStorage.setItem('list', JSON.stringify(this.ListObjects));
-        this.completedStausCheck();
-    }
+  deleteTask(listLi, trashIcon) {
+    listLi.remove();
+    this.ListObjects.splice(trashIcon.id, 1);
+    this.render();
+    localStorage.setItem('list', JSON.stringify(this.ListObjects));
+    this.completedStausCheck();
+  }
 
-    editTask(editEventTarget) {
-        editEventTarget.toggleAttribute('readonly');
-        editEventTarget.addEventListener('keyup', () => {
-            /* eslint-disable */
+  editTask(editEventTarget) {
+    editEventTarget.toggleAttribute('readonly');
+    editEventTarget.addEventListener('keyup', () => {
+      /* eslint-disable */
             for (const [i, item] of this.ListObjects.entries()) {
                 if (parseInt(editEventTarget.parentElement.id) === i) {
                     item.description = editEventTarget.value;
